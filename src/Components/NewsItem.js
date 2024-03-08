24231a5244352012a2f64c6a9e6e1f3ed3f32311
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-03-08 20:01:48 +0530

    Some minor changes and add the card view to display the news in the card

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,7 +2,21 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>This is a news items</div>;
+    let { title, description, image } = this.props;
+    return (
+      <div>
+        <div className="card">
+          <img src={image} className="card-img-top" alt={title} />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a href="/newsItem" className="btn btn-primary">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
